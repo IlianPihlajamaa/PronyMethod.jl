955d8f561d91678ed30d5c394f276f3d7d256cbf
Pihlajamaa <i.l.pihlajamaa@tue.nl>
2022-04-13 17:21:46 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,24 +12,24 @@ using Test, Prony
     end
 end
 
-# @testset "PronyLS" begin
-#     for N = 100:50:200
-#         x = collect(LinRange(0, 2, N))
-#         y = @. exp(-x*2)*cos(3x)+sin(2x)
-#         f  = prony(x,y,round(Int64,N/2-2))
-#         ynew = f.(x)
-#         error = maximum(abs.(y.-ynew))
-#         @test error < 10^-5
-#     end
-# end
+@testset "PronyLS" begin
+    for N = 100:50:200
+        x = collect(LinRange(0, 5, N))
+        y = @. exp(-x*2)*cos(3x)+sin(2x)
+        f  = prony(x,y,round(Int64,N/2-2))
+        ynew = f.(x)
+        error = maximum(abs.(y.-ynew))
+        @test error < 10^-5
+    end
+end
 
-# @testset "PronyMPM" begin
-#     for N = 100:50:200
-#         x = collect(LinRange(0, 2, N))
-#         y = @. exp(-x*2)*cos(3x)+sin(2x)
-#         f  = prony(x,y,round(Int64,N/2-10), PronyMethodMPM())
-#         ynew = f.(x)
-#         error = maximum(abs.(y.-ynew))
-#         @test error < 10^-5
-#     end
-# end
+@testset "PronyMPM" begin
+    for N = 100:50:200
+        x = collect(LinRange(0, 5, N))
+        y = @. exp(-x*2)*cos(3x)+sin(2x)
+        f  = prony(x,y,round(Int64,N/2-10), PronyMethodMPM())
+        ynew = f.(x)
+        error = maximum(abs.(y.-ynew))
+        @test error < 10^-5
+    end
+end
