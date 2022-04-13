5217743393e0d3576c99a1f37601dd0e8f96ae9e
Pihlajamaa <i.l.pihlajamaa@tue.nl>
2022-04-13 17:24:48 +0200

    test with bessel

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,10 @@
-using Test, Prony
+using Test, Prony, SpecialFunctions
 
 
 @testset "Prony" begin
     for N = 50:50:200
         x = collect(LinRange(0, 4, 50))
-        y = @. exp(-x*2)*cos(3x)+sin(2x)
+        y = @. besselj0(x)#exp(-x*2)*cos(3x)+sin(2x)
         f  = prony(x,y)
         ynew = f.(x)
         error = maximum(abs.(y.-ynew))
@@ -15,7 +15,7 @@ end
 @testset "PronyLS" begin
     for N = 100:50:200
         x = collect(LinRange(0, 5, N))
-        y = @. exp(-x*2)*cos(3x)+sin(2x)
+        y = @. besselj0(x)
         f  = prony(x,y,round(Int64,N/2-2))
         ynew = f.(x)
         error = maximum(abs.(y.-ynew))
@@ -26,7 +26,7 @@ end
 @testset "PronyMPM" begin
     for N = 100:50:200
         x = collect(LinRange(0, 5, N))
-        y = @. exp(-x*2)*cos(3x)+sin(2x)
+        y = @. besselj0(x)
         f  = prony(x,y,round(Int64,N/2-10), PronyMethodMPM())
         ynew = f.(x)
         error = maximum(abs.(y.-ynew))
