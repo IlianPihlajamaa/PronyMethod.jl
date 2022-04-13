372410519eba16c25dfecb3c56d5d59904f35c73
Pihlajamaa <i.l.pihlajamaa@tue.nl>
2022-04-13 17:08:05 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,35 @@
 using Test, Prony
 
 
-@testset begin
-    x = collect(LinRange(0, 5, 50))
-    y = @. exp(-x*2)*cos(3x)+sin(2x)
-    for f in [prony(x,y), prony(x,y, 20), prony(x,y, 20, PronyMethodMPM())]
+@testset "Prony" begin
+    for N = 50:50:200
+        x = collect(LinRange(0, 2, 50))
+        y = @. exp(-x*2)*cos(3x)+sin(2x)
+        f  = prony(x,y)
         ynew = f.(x)
         error = maximum(abs.(y.-ynew))
         @test error < 10^-5
     end
 end
+
+# @testset "PronyLS" begin
+#     for N = 100:50:200
+#         x = collect(LinRange(0, 2, N))
+#         y = @. exp(-x*2)*cos(3x)+sin(2x)
+#         f  = prony(x,y,round(Int64,N/2-2))
+#         ynew = f.(x)
+#         error = maximum(abs.(y.-ynew))
+#         @test error < 10^-5
+#     end
+# end
+
+# @testset "PronyMPM" begin
+#     for N = 100:50:200
+#         x = collect(LinRange(0, 2, N))
+#         y = @. exp(-x*2)*cos(3x)+sin(2x)
+#         f  = prony(x,y,round(Int64,N/2-10), PronyMethodMPM())
+#         ynew = f.(x)
+#         error = maximum(abs.(y.-ynew))
+#         @test error < 10^-5
+#     end
+# end
